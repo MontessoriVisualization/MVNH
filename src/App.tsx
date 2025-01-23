7abfe5a8317042b0eb1aa7005d4172fff19774c1
MontessoriVisualization <MontessoriVisualization@gmailcom>
2025-01-23 14:44:20 +0545

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import "./index.css";
+import "./index.css";
 // import ClassesGrid from "./Layouts/body/class11Grid";
-// import CardsGrid from "./Layouts/body/CardsGrid";
-// import CatGrid from "./Layouts/body/catGrid";
-// import FaqGrid from "./Layouts/body/faqGrid";
-// import FooterGrid from "./Layouts/body/FooterGrid";
-// import Hero from "./Layouts/body/hero";
-// import Header from "./Layouts/header/Header";
+import CardsGrid from "./Layouts/body/CardsGrid";
+import CatGrid from "./Layouts/body/catGrid";
+import FaqGrid from "./Layouts/body/faqGrid";
+import FooterGrid from "./Layouts/body/FooterGrid";
+import Hero from "./Layouts/body/hero";
+import Header from "./Layouts/header/Header";
 // import Login from "./Layouts/login/MainLogin";
 
 function App() {
   return (
     <>
-      {/* <Header></Header> */}
-      {/* <Hero></Hero>
+      <Header></Header>
+      <Hero></Hero>
       <CardsGrid />
       <CatGrid></CatGrid>
       <FaqGrid></FaqGrid>
-      <FooterGrid></FooterGrid> */}
+      <FooterGrid></FooterGrid>
       {/* <ClassesGrid></ClassesGrid> */}
       {/* <Login></Login> */}
     </>
