3cfbc97e33ee402ae98528f688cf1df9820d5730
Saugat <MontessoriVisualization@users.noreply.github.com>
2025-11-30 23:21:02 +0545

    Small correction removed twitter from socials

--- src/Layouts/body/FooterGrid.tsx
+++ src/Layouts/body/FooterGrid.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../../Component/Footer";
 import Logo from "./logo.svg";
-import { Linkedin, Twitter, Facebook, Instagram, Youtube } from "lucide-react";
+import { Linkedin, Facebook, Instagram, Youtube } from "lucide-react";
 import Button from "../../Component/button";
 import { PiTiktokLogo } from "react-icons/pi";
 
